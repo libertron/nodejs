42d47289d6f94132d0297cc5278ae23acb4a4cda
libertron <marcusserginio96@gmail.com>
2019-07-12 21:39:59 -0400

    render response as result

--- rtfd ReadTheFuck'nDoc/express_basic.js
+++ rtfd ReadTheFuck'nDoc/express_basic.js
@@ -1,12 +1,11 @@
 var express=require('express');
 var app=express();
 
-app.use('/user/:id',function(req,res,next){
-    console.log('Request URL : ',req.originalUrl);
-    next();
-},function(req,res,next){
-    console.log('Request Type : ',req.method);
-    next();
+app.get('/user/:id', function (req, res, next) {
+    console.log('ID:', req.params.id)
+    next()
+  }, function (req, res, next) {
+    res.send('Hello World')
 });
 
 app.listen(9090);
