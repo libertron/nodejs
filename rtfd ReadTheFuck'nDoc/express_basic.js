d3dbab1866ab79eb994bdbc449ebc724512586f6
libertron <marcusserginio96@gmail.com>
2019-07-12 21:02:51 -0400

    mounting multiple midlewares at a specific point. : notice that without next() some incoming functions may not triggered

--- rtfd ReadTheFuck'nDoc/express_basic.js
+++ rtfd ReadTheFuck'nDoc/express_basic.js
@@ -1,8 +1,11 @@
 var express=require('express');
 var app=express();
 
-app.use(function(req,res,next){
-    console.log('Time:', Date.now());
+app.use('/user/:id',function(req,res,next){
+    console.log('Request URL : ',req.originalUrl);
+    next();
+},function(req,res,next){
+    console.log('Request Type : ',req.method);
     next();
 });
 
