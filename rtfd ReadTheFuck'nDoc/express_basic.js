c7efec62cbb15c297250f42c1b9906dbe6fb1070
libertron <marcusserginio96@gmail.com>
2019-07-12 20:40:31 -0400

    using callback without send method : notice the app keep loading after resulting

--- rtfd ReadTheFuck'nDoc/express_basic.js
+++ rtfd ReadTheFuck'nDoc/express_basic.js
@@ -1,9 +1,8 @@
 var express=require('express');
 var app=express();
 
-app.get('/test',function(req,res,next){
-    console.log('Request-type : ',req.method);
-    res.send('USER');
+app.use(function(req,res,next){
+    console.log('Time:', Date.now());
 });
 
 app.listen(9090);
