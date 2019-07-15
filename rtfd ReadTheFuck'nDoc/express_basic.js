e17bb33cdf0a1f477dab484ffd453e7209f6c112
libertron <marcusserginio96@gmail.com>
2019-07-15 14:36:00 -0400

    handling hand point with parameters. Notice by the way that i log the res object

--- rtfd ReadTheFuck'nDoc/express_basic.js
+++ rtfd ReadTheFuck'nDoc/express_basic.js
@@ -1,15 +1,10 @@
 var express=require('express');
 var app=express();
 
-app.get('/user/:id', function (req, res, next) {
-    console.log('ID:', req.params.id)
-    next()
-  }, function (req, res, next) {
-    res.send('Hello World')
-});
-
-app.get('/user/:id', function (req, res, next) {
-    res.end(req.params.id)
+app.get('/users/:id/:name',function(req,res,next){
+  console.log(req.params);
+  console.log(res);
+  next();
 });
 
 app.listen(9090);
