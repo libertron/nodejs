504928cf4a3d001d7aa86926820f631d6c56b706
libertron <marcusserginio96@gmail.com>
2019-07-15 15:30:15 -0400

    first added a frontend folder for testing node server api and secondly added permission for CORS request and finally chaining handler using 'app.route()' function

--- rtfd ReadTheFuck'nDoc/express_basic.js
+++ rtfd ReadTheFuck'nDoc/express_basic.js
@@ -1,6 +1,15 @@
 var express=require('express');
 var app=express();
 
+//to solve CORS errors
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
 var cb0 = function (req, res, next) {
   console.log('CB0')
   next()
@@ -16,7 +25,7 @@ var cb2 = function (req, res,next) {
   next();
 }
 
-app.get('/users/:id/:name',[cb0,cb1,cb2], function (req, res, next) {
+app.route('/users/:id/:name').get([cb0,cb1,cb2], function (req, res, next) {
   console.log('the response will be sent by the next function ...')
   next()
 }, function (req, res) {
