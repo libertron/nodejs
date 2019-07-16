83e1f57b6bf69fa61bdacdcafa971a5d4de48f20
libertron <marcusserginio96@gmail.com>
2019-07-15 21:41:03 -0400

    doing the same thing og app.method() with router.method()

--- rtfd ReadTheFuck'nDoc/express_basic.js
+++ rtfd ReadTheFuck'nDoc/express_basic.js
@@ -1,5 +1,6 @@
 var express=require('express');
 var app=express();
+var router=express.Router();
 
 //to solve CORS errors
 app.use(function(req, res, next) {
@@ -9,35 +10,39 @@ app.use(function(req, res, next) {
   next();
 });
 
-var logger = function (req, res, next) {
-  console.log('LOGGED')
-  next();
-};
-
-var requestTime=function(req,res,next){
-  req.requestTime=Date.now;
-  next();
-};
-
-app.use(logger);
-app.use(requestTime);
-
-app.get('/',function(req,res){
-  var htmlResponse='hello! <br/> you request that page at: ';
-  htmlResponse+='<smal>'+req.requestTime+'</smal>';
-  res.send(htmlResponse);
-});
-
-app.get('/user/:id',function(req,res,next){
-  if(req.params.id==='0') next('route')
-  else
-    next();
-},function(req,res,next){
-  res.send('Case that req.id >0');
-});
-
-app.get('/user/:id',function(req,res,next){
-  res.send('In that handler function req.id might equal to 0. Result =>'+req.params.id);
+// a middleware function with no mount path. This code is executed for every request to the router
+router.use(function (req, res, next) {
+  console.log('Time:', Date.now())
+  next()
 })
 
+// a middleware sub-stack shows request info for any type of HTTP request to the /user/:id path
+router.use('/user/:id', function (req, res, next) {
+  console.log('Request URL:', req.originalUrl)
+  next()
+}, function (req, res, next) {
+  console.log('Request Type:', req.method)
+  next()
+})
+
+// a middleware sub-stack that handles GET requests to the /user/:id path
+router.get('/user/:id', function (req, res, next) {
+  // if the user ID is 0, skip to the next router
+  if (req.params.id === '0') next('route')
+  // otherwise pass control to the next middleware function in this stack
+  else next()
+}, function (req, res, next) {
+  // render a regular page
+  // res.render('../page.ejs')
+  res.send('ok');
+})
+
+// handler for the /user/:id path, which renders a special page
+router.get('/user/:id', function (req, res, next) {
+  console.log(req.params.id)
+  res.render('special')
+})
+
+// mount the router on the app
+app.use('/', router)  
 app.listen(9090);
